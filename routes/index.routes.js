ef996574cd19bab1809d5a38398ec9e25879a360
Precious Ibeagi <preciousaang@gmail.com>
2022-03-18 07:59:04 +0100

    fixed bug on fees upload

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,7 +6,9 @@ router.post("/fees", (req, res) => {
   const pattern =
     /^(?<feeID>LNPY\d{4,}) (?<feeCurrency>[A-Z]+) (?<feeLocale>LOCL|INTL|\*) (?<feeEntity>CREDIT-CARD|DEBIT-CARD|BANK-ACCOUNT|USSD|WALLET-ID|\*)\((?<entityProperty>\*|[A-z]+)\) : APPLY (?<feeType>PERC|FLAT_PERC|FLAT) (?<feeValue>(\d+.?\d*:\d+.?\d+|\d+.?\d*))$/g;
 
-  const specs = req.body.FeeConfigurationSpec.split("\\n");
+  const specs = req.body.FeeConfigurationSpec.replaceAll("\\n", "\n").split(
+    "\n"
+  );
   for (const spec of specs) {
     let match = pattern.exec(spec);
     do {
--- test/fees.test.js
+++ test/fees.test.js
@@ -17,7 +17,7 @@ describe("/POST Fees", function () {
       .post("/fees")
       .send({
         FeeConfigurationSpec:
-          "LNPY1221 NGN * *(*) : APPLY PERC 1.4\\nLNPY1222 NGN INTL CREDIT-CARD(VISA) : APPLY PERC 5.0\\nLNPY1223 NGN LOCL CREDIT-CARD(*) : APPLY FLAT_PERC 50:1.4\\nLNPY1224 NGN * BANK-ACCOUNT(*) : APPLY FLAT 100\\nLNPY1225 NGN * USSD(MTN) : APPLY PERC 0.55",
+          "LNPY1221 NGN * *(*) : APPLY PERC 1.4\nLNPY1222 NGN INTL CREDIT-CARD(VISA) : APPLY PERC 5.0\nLNPY1223 NGN LOCL CREDIT-CARD(*) : APPLY FLAT_PERC 50:1.4\nLNPY1224 NGN * BANK-ACCOUNT(*) : APPLY FLAT 100\nLNPY1225 NGN * USSD(MTN) : APPLY PERC 0.55",
       })
       .end((err, res) => {
         should.not.exist(err);
